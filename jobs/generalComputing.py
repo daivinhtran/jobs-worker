eff325092aa2ba6e6e34417056e839bfa9a521f5
Yunbo Zhang <yzhang3027@joe-6.pace.gatech.edu>
2017-04-03 14:31:56 -0400

    Modified file names and add espress calculation into metadata

--- jobs/generalComputing.py
+++ jobs/generalComputing.py
@@ -23,15 +23,7 @@ def parseSysParam(argv):
     for k, v in ((k.lstrip('-'), v) for k,v in (a.split('=') for a in argv)):
         d[k].append(v)
     return dict(d)
-
-def fermi_dirac(args):
-    epsilon = float(args['eps'][0])
-    mu = float(args['mu'][0])
-    T = float(args['t'][0])
-    kB = 8.615e-5 #eV/K (we will typically use energy units of eV)
-    n = 1.0 /(np.exp((epsilon - mu)/(kB * T)) + 1.0)
-    return n
-
+    
 if __name__ == "__main__":
     args = parseSysParam(sys.argv[1:])
     main(args)
